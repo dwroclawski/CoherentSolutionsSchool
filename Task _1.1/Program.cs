ab59685afdddabc31248e665bab27acd629bd676
David Wrocławski <david.wroclawski@gmail.com>
2024-10-13 16:38:31 +0200

    task 1.2

--- Task _1.1/Program.cs
+++ Task _1.1/Program.cs
@@ -4,51 +4,26 @@ namespace FirstTask
 {
     internal class Program
     {
-        static string IntToDuodecimal(int number)
-        {
-            if (number == 0)
-                return "0";
-
-            string digits = "0123456789AB"; 
-            string result = "";
-            int remainder;
-
-            while (number > 0)
-            {
-                remainder = number % 12;
-                result = digits[remainder] + result; 
-                number /= 12;
-            }
-
-            return result;
-        }
-
-        static int CountA(string number)
-        {
-            int count = 0;
-            foreach (char c in number)
-            {
-                if (c == 'A') count++;
-            }
-            return count;
-        }
-
         static void Main(string[] args)
         {
-            Console.WriteLine("Enter the first number:");
-            int firstNumber = int.Parse(Console.ReadLine());
-            Console.WriteLine("Enter the second number:");
-            int secondNumber = int.Parse(Console.ReadLine());
+            Console.WriteLine("Enter the first 9 digits:");
+            string isbn = Console.ReadLine();
 
-            for (int i = firstNumber; i <= secondNumber; i++) 
-            { 
-                string duodecimal = IntToDuodecimal(i);
-                if(CountA(duodecimal) == 2)
-                {
-                    Console.WriteLine($"{i} ({duodecimal})");
-                }
+            int control = 0;
+            for (int i = 0; i < 9; i++)
+            {
+               control = int.Parse(isbn[i].ToString()) * (10-i);
             }
+            control = control % 11;
 
+            if (control == 10)
+            {
+                Console.WriteLine("The ISBN-10 number is: " + isbn + "X");
+            }
+            else
+            {
+                Console.WriteLine("The ISBN-10 number is: " + isbn + control);
+            }
         }
     }
 }
